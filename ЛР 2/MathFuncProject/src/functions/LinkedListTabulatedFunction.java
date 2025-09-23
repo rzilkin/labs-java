e3cc49f917bfafa6f994260b5d913ebf3392c681
GamingPC <fromenlesan2@gmail.com>
2025-09-23 23:31:43 +0300

    creating two constructors for linkedlisttabulatedfunction

--- ЛР 2/MathFuncProject/src/functions/LinkedListTabulatedFunction.java
+++ ЛР 2/MathFuncProject/src/functions/LinkedListTabulatedFunction.java
@@ -34,4 +34,31 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
 
         count += 1;
     }
+
+    public LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
+        for(int i = 0; i < xValues.length; ++i) {
+            addNode(xValues[i], yValues[i]);
+        }
+    }
+
+    public LinkedListTabulatedFunction(MathFunction source, double xFrom, double xTo, int count) {
+        if(xFrom > xTo) {
+            double temp = xFrom;
+            xFrom = xTo;
+            xTo = temp;
+        }
+
+        if(xFrom == xTo) {
+            for (int i = 0; i < count; ++i) {
+                addNode(xFrom, source.apply(xFrom));
+            }
+        }
+        else {
+            double step = (xTo - xFrom) / (count - 1);
+            for(int i = 0; i < count; ++i) {
+                double x = xFrom + i * step;
+                addNode(x, source.apply(x));
+            }
+        }
+    }
 }
