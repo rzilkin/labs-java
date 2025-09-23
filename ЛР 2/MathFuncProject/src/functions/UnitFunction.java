ba270103298fbe4740a5ee35ebbe6c7b01033cd2
Zhilkin Roman <rzilkin1@gmail.com>
2025-09-24 01:35:04 +0400

    added ConstantFunction, ZeroFunction, UnitFunction, tests

---
+++ ЛР 2/MathFuncProject/src/functions/UnitFunction.java
@@ -0,0 +1,8 @@
+package functions;
+
+// Функция, всегда возвращающая 1.
+public class UnitFunction extends ConstantFunction{
+    public UnitFunction(){
+        super(1.0); // Вызов конструктора родителя
+    }
+}
---
+++ ЛР 2/MathFuncProject/src/functions/ZeroFunction.java
@@ -0,0 +1,8 @@
+package functions;
+
+// Функция, всегда возвращающая 0.
+public class ZeroFunction extends ConstantFunction{
+    public ZeroFunction(){
+        super(0.0); // Вызов конструктора родителя
+    }
+}
