9b101d88a8cbf92913e868f527906aa6c86da65f
GamingPC <fromenlesan2@gmail.com>
2025-09-24 20:02:50 +0300

    creating of the remaining methods and methods of extrapolation and interpolation and creation of tests for linkedlisttabulatedfunction

--- ЛР 2/MathFuncProject/src/functions/LinkedListTabulatedFunction.java
+++ ЛР 2/MathFuncProject/src/functions/LinkedListTabulatedFunction.java
@@ -94,4 +94,83 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
         }
     }
 
+    @Override
+    public double getX(int index) {
+        return getNode(index).x;
+    }
+
+    @Override
+    public double getY(int index) {
+        return getNode(index).y;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        getNode(index).y = value;
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        Node cur = head;
+        for (int i = 0; i < count; i++) {
+            if (cur.x == x) {
+                return i;
+            }
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        Node cur = head;
+        for (int i = 0; i < count; i++) {
+            if (cur.y == y) {
+                return i;
+            }
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int floorIndexOfX(double x) {
+        if (x < head.x) return 0;
+
+        Node cur = head;
+        for (int i = 0; i < count; i++) {
+            if (cur.x > x) {
+                return i == 0 ? 0 : i - 1;
+            }
+            cur = cur.next;
+        }
+        return count;
+    }
+
+    @Override
+    public double extrapolateLeft(double x) {
+        if (count == 1) return head.y;
+        Node first = head;
+        Node second = head.next;
+        return interpolate(x, first.x, second.x, first.y, second.y);
+    }
+
+    @Override
+    public double extrapolateRight(double x) {
+        if (count == 1) return head.y;
+        Node last = head.prev;
+        Node secondLast = last.prev;
+        return interpolate(x, secondLast.x, last.x, secondLast.y, last.y);
+    }
+
+    @Override
+    public double interpolate(double x, int floorIndex) {
+        if (count == 1) return head.y;
+        if (floorIndex == count) return head.prev.y;
+
+        Node left = getNode(floorIndex);
+        Node right = left.next;
+        return interpolate(x, left.x, right.x, left.y, right.y);
+    }
+
 }
---
+++ ЛР 2/MathFuncProject/src/test/LinkedListTabulatedFunctionTest.java
@@ -0,0 +1,131 @@
+package test;
+
+import functions.LinkedListTabulatedFunction;
+import functions.MathFunction;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTabulatedFunctionTest {
+
+    private LinkedListTabulatedFunction createFunctionWithArrays() {
+        double[] xValues = {3.0, 5.0, 8.0, 12.0};
+        double[] yValues = {1.0, 12.0, 15.0, 30.0};
+        return new LinkedListTabulatedFunction(xValues, yValues);
+    }
+
+    private LinkedListTabulatedFunction createFunctionWithMathFunction() {
+        MathFunction source = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+        return new LinkedListTabulatedFunction(source, 2, 10, 7);
+    }
+
+    @Test
+    void testConstructorWithArrays() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(4, func.getCount());
+        assertEquals(3.0, func.leftBound());
+        assertEquals(12.0, func.rightBound());
+        assertEquals(12.0, func.getY(1));
+    }
+
+    @Test
+    void testConstructorFromMathFunction() {
+        LinkedListTabulatedFunction func = createFunctionWithMathFunction();
+        assertEquals(7, func.getCount());
+        assertEquals(1.0, func.leftBound());
+        assertEquals(7.0, func.rightBound());
+        assertEquals(1.0, func.getY(0));
+        assertEquals(25.0, func.getY(4));
+    }
+
+    @Test
+    void testGetCount() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(4, func.getCount());
+    }
+
+    @Test
+    void testLeftBound() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(3.0, func.leftBound());
+    }
+
+    @Test
+    void testRightBound() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(12.0, func.rightBound());
+    }
+
+    @Test
+    void testGetX() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(8.0, func.getX(2));
+        assertEquals(5.0, func.getX(1));
+    }
+
+    @Test
+    void testGetY() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(1.0, func.getY(0));
+        assertEquals(30.0, func.getY(3));
+    }
+
+    @Test
+    void testSetY() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        func.setY(1, 234.0);
+        assertEquals(234.0, func.getY(1));
+        assertEquals(1.0, func.getY(0));
+        assertEquals(15.0, func.getY(2));
+        assertEquals(30.0, func.getY(3));
+    }
+
+    @Test
+    void testIndexOfX() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(3, func.indexOfX(12.0));
+        assertEquals(-1, func.indexOfX(7.467));
+    }
+
+    @Test
+    void testIndexOfY() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(-1, func.indexOfY(1982.456));
+        assertEquals(2, func.indexOfY(15.0));
+    }
+
+    void testFloorIndexOfX() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        assertEquals(0, func.floorIndexOfX(3.78));
+        assertEquals(1, func.floorIndexOfX(5.67));
+        assertEquals(2, func.floorIndexOfX(8.0));
+        assertEquals(0, func.floorIndexOfX(1.5));
+        assertEquals(2, func.floorIndexOfX(12.3));
+    }
+
+    @Test
+    void testInterpolate() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        double res = func.interpolate(6.5, 1);
+        assertEquals(13.5, res, 1e-6);
+    }
+
+    @Test
+    void testExtrapolateLeft() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        double res = func.extrapolateLeft(1.5);
+        assertEquals(-7.25, res, 1e-6);
+    }
+
+    @Test
+    void testExtrapolateRight() {
+        LinkedListTabulatedFunction func = createFunctionWithArrays();
+        double res = func.extrapolateRight(13.4);
+        assertEquals(35.25, res, 1e-6);
+    }
+
+}
