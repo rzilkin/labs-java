4f22382cf5181a6697a07a9cd9ef9e0b35ac69eb
Zhilkin Roman <rzilkin1@gmail.com>
2025-10-07 21:01:07 +0400

    the completed FunctionsIO class has been created in the io package

---
+++ ЛР 3/MathFuncProject/src/main/java/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Экземпляры у данного класса не допускаются.");
+    }
+}
