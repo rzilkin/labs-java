de1cc604a28aa4bf9121feb4584d8a372f5407e8
Zhilkin Roman <rzilkin1@gmail.com>
2025-10-09 23:22:53 +0400

    The writeTabulatedFunction method is implemented in FunctionsIO, which writes a representation of a function to a buffered writer character stream. The io package has created the TabulatedFunctionFileWriter class with the main method

--- ЛР 3/MathFuncProject/src/main/java/io/FunctionsIO.java
+++ ЛР 3/MathFuncProject/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,24 @@
 package io;
 
+import functions.TabulatedFunction;
+import functions.Point;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Экземпляры у данного класса не допускаются.");
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter out = new PrintWriter(writer, true);
+        out.println(function.getCount());
+        for (Point p : function){
+            out.printf("%f %f\n", p.x, p.y);
+        }
+        try{
+            writer.flush();
+        } catch(IOException e){}
     }
 }
