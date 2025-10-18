588c5f6791aada2b9f2f3692c77c5b817ae40b87
Zhilkin Roman <rzilkin1@gmail.com>
2025-10-18 22:07:58 +0400

    the concurrent package has been created. A classMultiplyingTask has been created that implements the Runnable interface, which doubles all y values

---
+++ ЛР 4/MathFuncProject/src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        int n = function.getCount();
+        for(int i = 0; i < n; i++) {
+            double y = function.getY(i);
+            function.setY(i, y * 2.0);
+        }
+
+        System.out.println("Поток " + Thread.currentThread().getName() + " закончил выполнение задачи");
+    }
+}
