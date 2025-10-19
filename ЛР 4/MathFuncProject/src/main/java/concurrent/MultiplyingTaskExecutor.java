c4e784a7ce9e4efc01428c34ed730feb777d9d2b
Zhilkin Roman <rzilkin1@gmail.com>
2025-10-19 21:13:44 +0400

    The MultiplyingTaskExecutor class has been created with a main method that runs 10 threads in parallel

---
+++ ЛР 4/MathFuncProject/src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+import functions.UnitFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyingTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction func = new LinkedListTabulatedFunction(new UnitFunction(), 1.0, 1000.0, 1000);
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new MultiplyingTask(func));
+            threads.add(t);
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        Thread.sleep(2000);
+
+        System.out.println(func.toString());
+    }
+}
