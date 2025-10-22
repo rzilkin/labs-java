9bd7ad154623a513a363869fc8d3095080d28710
Алексей <lyosha.fomin.06@mail.ru>
2025-10-22 21:25:13 +0400

    added readTask class and overloaded run method

---
+++ src/main/java/concurrent/ReadTask.java
@@ -0,0 +1,24 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class ReadTask implements Runnable {
+    private final TabulatedFunction func;
+
+    public ReadTask(TabulatedFunction func) {
+        this.func = func;
+    }
+
+    @Override
+    public void run() {
+        int count = func.getCount();
+
+        for (int i = 0; i < count; ++i) {
+            double x = func.getX(i);
+            double y = func.getY(i);
+
+            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y);
+        }
+    }
+
+}
