28750ba9b3d0b712a12990221975c831d077d0cc
Алексей <lyosha.fomin.06@mail.ru>
2025-10-22 22:55:56 +0400

    added writeTask class and overloaded run method

---
+++ src/main/java/concurrent/WriteTask.java
@@ -0,0 +1,25 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction func;
+    private final double value;
+
+    WriteTask(TabulatedFunction func, double value) {
+        this.func = func;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        int count = func.getCount();
+
+        for(int i = 0; i < count; ++i) {
+            double x = func.getX(i);
+            func.setY(i, value);
+
+            System.out.printf("Writing for index %d complete\n", i);
+        }
+    }
+}
