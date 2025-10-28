afc1a51dfd99580f9a16856b105798ac4887be9b
Алексей <lyosha.fomin.06@mail.ru>
2025-10-28 18:55:03 +0400

    Deleted mutex from SyncronizedTabulatedFunction

--- src/main/java/concurrent/SynchronizedTabulatedFunction.java
+++ src/main/java/concurrent/SynchronizedTabulatedFunction.java
@@ -7,7 +7,6 @@ import java.util.Iterator;
 
 public class SynchronizedTabulatedFunction implements TabulatedFunction {
     private final TabulatedFunction func;
-    private final Object mutex;
 
     public interface Operation<T> {
         T apply(SynchronizedTabulatedFunction sync);
@@ -15,95 +14,72 @@ public class SynchronizedTabulatedFunction implements TabulatedFunction {
 
     public SynchronizedTabulatedFunction(TabulatedFunction func) {
         this.func = func;
-        this.mutex = this;
     }
 
-    public <T> T doSynchronously(Operation<? extends T> op) {
-        synchronized (mutex) {
-            return op.apply(this);
-        }
+    public synchronized  <T> T doSynchronously(Operation<? extends T> op) {
+        return op.apply(this);
     }
 
     @Override
-    public int getCount() {
-        synchronized (mutex) {
-            return func.getCount();
-        }
+    public synchronized int getCount() {
+        return func.getCount();
     }
 
     @Override
-    public double getX(int index) {
-        synchronized (mutex) {
-            return func.getX(index);
-        }
+    public synchronized double getX(int index) {
+        return func.getX(index);
     }
 
     @Override
-    public double getY(int index) {
-        synchronized (mutex) {
-            return func.getY(index);
-        }
+    public synchronized double getY(int index) {
+        return func.getY(index);
     }
 
     @Override
-    public void setY(int index, double value) {
-        synchronized (mutex) {
-            func.setY(index, value);
-        }
+    public synchronized void setY(int index, double value) {
+        func.setY(index, value);
     }
 
     @Override
-    public int indexOfX(double x) {
-        synchronized (mutex) {
-            return func.indexOfX(x);
-        }
+    public synchronized int indexOfX(double x) {
+        return func.indexOfX(x);
     }
 
     @Override
-    public int indexOfY(double y) {
-        synchronized (mutex) {
-            return func.indexOfY(y);
-        }
+    public synchronized int indexOfY(double y) {
+        return func.indexOfY(y);
     }
 
     @Override
-    public double leftBound() {
-        synchronized (mutex) {
-            return func.leftBound();
-        }
+    public synchronized double leftBound() {
+        return func.leftBound();
     }
 
     @Override
-    public double rightBound() {
-        synchronized (mutex) {
-            return func.rightBound();
-        }
+    public synchronized double rightBound() {
+        return func.rightBound();
     }
 
     @Override
-    public Iterator<Point> iterator() {
-        synchronized (mutex) {
-            Point[] points = TabulatedFunctionOperationService.asPoints(func);
-            return new Iterator<Point>() {
-                private int curIndex = 0;
+    public synchronized Iterator<Point> iterator() {
+        Point[] points = TabulatedFunctionOperationService.asPoints(func);
+        return new Iterator<Point>() {
+            private int curIndex = 0;
 
-                @Override
-                public boolean hasNext() {
-                    return curIndex < points.length;
-                }
+            @Override
+            public boolean hasNext() {
+                return curIndex < points.length;
+            }
 
-                @Override
-                public Point next() {
-                    return points[curIndex++];
-                }
-            };
-        }
+            @Override
+            public Point next() {
+                return points[curIndex++];
+            }
+        };
     }
 
     @Override
-    public double apply(double x) {
-        synchronized (mutex) {
-            return func.apply(x);
-        }
+    public synchronized double apply(double x) {
+        return func.apply(x);
     }
 }
